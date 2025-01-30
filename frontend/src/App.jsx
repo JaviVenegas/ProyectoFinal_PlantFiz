09766f888499705f3c556404b2bafe891ee214a1
Daniel Molina <Danielmolina9202@gmail.com>
2025-01-30 20:57:07 -0300

    Cambio en el RouterManager para manejar todo desde ahí

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { RouterManager } from './router/RouterManager'
-import { CustomNav } from './components/Nav'
+
 // Aqui se declaran los import como el del carrito de compras
 
 function App() {
   return (
     <>
-      <Router>
-        <CustomNav />
-        <RouterManager />
-      </Router>    
+     <RouterManager />
     </>
   )
 }
--- frontend/src/router/RouterManager.jsx
+++ frontend/src/router/RouterManager.jsx
@@ -1,31 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import { Home} from "../pages/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from "../pages/Home";
 import { AdminDashboard } from "../pages/AdminDashboard";
 import { ProductCatalog } from "../pages/ProductCatalog";
 import { ProductDetails } from "../pages/ProductDetails";
 import { NotFound } from "../pages/NotFound";
-
-// Si tienes rutas protegidas (ej: solo para administradores), importa el guard aquí
-// import { AuthGuard } from "../guard/AuthGuard";
+import { CustomNav } from '../components/Nav';
+import { Footer } from '../components/Footer';
 
 export const RouterManager = () => {
   return (
-    <Routes>
-      {/* Ruta pública: Página de inicio */}
-      <Route path="/" element={<Home />} />
+    <Router>
+      {/* Navbar visible en todas las páginas */}
+      <CustomNav />
 
-      {/* Ruta protegida: Panel de administración */}
-      {/* <Route path="/admin" element={<AuthGuard><AdminDashboard /></AuthGuard>} /> */}
-      <Route path="/admin" element={<AdminDashboard />} />
+      {/* Contenedor de las rutas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/catalog" element={<ProductCatalog />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
-      {/* Ruta pública: Catálogo de productos */}
-      <Route path="/catalog" element={<ProductCatalog />} />
-
-      {/* Ruta dinámica: Detalles de un producto */}
-      <Route path="/product/:id" element={<ProductDetails />} />
-
-      {/* Ruta 404 */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+      {/* Footer visible en todas las páginas */}
+      <Footer />
+    </Router>
   );
 };
