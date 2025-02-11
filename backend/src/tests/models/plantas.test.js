d9c59ad4a458d2b8d18e452f624b5e46f2931a4f
Javiera Venegas <javieravenegas.arch@gmail.com>
2025-02-11 13:12:19 -0300

    iniciando implementacion a test

---
+++ backend/src/tests/models/plantas.test.js
@@ -0,0 +1,9 @@
+const {    //funciones a testear 
+      obtenerPlantas,
+    agregarPlanta, 
+    editarPlanta,
+    eliminarPlanta, 
+    ObtenerPlantaPorId, 
+    existe
+} = require('../../models/plantas');
+const { DB } = require('../../config/db');  //conecta a base de datos 
--- backend/src/tests/plantas.spec.js
+++ backend/src/tests/plantas.spec.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
-const app = require("..src/index");
+
+const app = require("../../src/app");
 
 describe("Operaciones CRUD de productos plantas", () => {
 
